46b1756c15f21de84f32b03bd916a6a3297f1908
jakirkham <jakirkham@gmail.com>
2021-04-14 09:40:35 -0700

    Enable POSIX API on Windows

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -11,6 +11,7 @@ cmake -G "NMake Makefiles" ^
       -DCMAKE_BUILD_TYPE:STRING=%CMAKE_CONFIG% ^
       -DBUILD_SHARED_LIBS:BOOL=ON ^
       -DBUILD_STATIC_LIBS:BOOL=ON ^
+      -DENABLE_POSIX_API:BOOL=ON ^
       -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
       "%SRC_DIR%"
 if errorlevel 1 exit 1
